16b9aa7bf8a592c982f425830f6454d8f9aa1697
Mayank Kakkar <121848593+MayankKakkar-004@users.noreply.github.com>
2025-04-28 21:05:16 +0530

    Time: 4 ms (24.91%) | Memory: 20.7 MB (27.79%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < nums.size(); i++)
-            if (nums[i - 1] == nums[i])
-                nums[i] = nums[i - 1] = INT_MIN;
-        return *max_element(nums.begin(), nums.end());
+        int n=nums.size();
+
+        int xorr=0;
+        for(int i =0;i<n;i++){
+            xorr=xorr ^ nums[i];
+
+        }
+        return xorr;
     }
 };
